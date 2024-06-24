4314bfecece5e34052c9bf18053e3d55807aa4fa
ivan kuzin <ivan.kuzin@ptnl.moscow>
2024-06-24 17:02:39 +0300

    features/steps/preconditional.py Поменял местами степы, для корректной работы

--- features/steps/preconditional.py
+++ features/steps/preconditional.py
@@ -6,6 +6,32 @@ from helpers.prepare import prepare_text
 use_step_matcher("cfparse")
 
 
+@Given('Я открываю сайт "{url}", "{login}"/"{password}"')
+def step_visit_with_auth(context, url, login, password):
+    context.page = context.playwright_context.new_page()
+    page = ClassAction(context)
+    context.host = url
+    a = Api(context.host)
+    response = a.post(path="/api/v1/login",
+                      json={
+                          "login": login,
+                          "password": password,
+                          "is_remember": True
+                      }
+                      )
+    token = response.json()["access_token"]
+    refresh_token = response.json()["refresh_token"]
+    context.playwright_context.add_cookies([
+        {"name": "access_token", "value": token,
+         'domain': context.host.replace('https://', '').replace('http://', '').replace('/', ''),
+         'path': '/'},
+        {"name": "refresh_token", "value": refresh_token,
+         'domain': context.host.replace('https://', '').replace('http://', '').replace('/', ''),
+         'path': '/'}
+    ])
+    page.visit(url)
+
+
 @Given('Я открываю сайт "{url}"')
 def step_visit(context, url):
     context.page = context.playwright_context.new_page()
@@ -25,29 +51,3 @@ def step_create_entity_without_name(context):
     entity_name = prepare_text("random")
     api = Api(context.host)
     api.post(path="avi/v1/entities", json={"name": entity_name})
-
-
-@Given('Я открываю сайт "{url}", "{login}"/"{password}"')
-def step_visit_with_auth(context, url, login, password):
-    context.page = context.playwright_context.new_page()
-    page = ClassAction(context)
-    context.host = url
-    a = Api(context.host)
-    response = a.post(path="/api/v1/login",
-                      json={
-                            "login": login,
-                            "password": password,
-                            "is_remember": True
-                            }
-        )
-    token = response.json()["access_token"]
-    refresh_token = response.json()["refresh_token"]
-    context.playwright_context.add_cookies([
-        {"name": "access_token", "value": token,
-         'domain': context.host.replace('https://', '').replace('http://', '').replace('/', ''),
-         'path': '/'},
-        {"name": "refresh_token", "value": refresh_token,
-         'domain': context.host.replace('https://', '').replace('http://', '').replace('/', ''),
-         'path': '/'}
-    ])
-    page.visit(url)
